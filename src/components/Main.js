3eccfd0be92ae870613cbfd9cb3a4668aa5bb92f
PatrickVm <patrick.v.murray@gmail.com>
2020-09-07 18:07:11 -0500

    edit #4 9/7 try 3 for configuration of email/contact

--- src/components/Main.js
+++ src/components/Main.js
@@ -92,7 +92,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="https://getform.io/f/82d41884-1572-4f95-bd6d-bbcf792258ab">
+          <form name="contact" method="post" data-netlify="true">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
