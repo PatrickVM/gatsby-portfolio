2d1623b0297f3ba3151136ab6c98130939503540
PatrickVm <patrick.v.murray@gmail.com>
2020-09-07 10:41:29 -0500

    edit #2 9/7

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Jordan Allen - Web Developer',
+    title: 'Patrick Murray - Web Developer',
     author: 'Hunter Chang',
     description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
   },
