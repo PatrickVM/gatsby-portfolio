f5502a95aca8501866212bedd17f66a9fe52f13b
PatrickVm <patrick.v.murray@gmail.com>
2020-09-07 18:14:21 -0500

    edit #5 9/7 configuring email/contact

--- src/components/Main.js
+++ src/components/Main.js
@@ -92,7 +92,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form name="contact" method="post" data-netlify="true">
+          <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
